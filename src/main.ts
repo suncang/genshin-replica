21b5f4f97edf19f7cce42ab861e2132f6abb63ff
suzuranXD <suncang4@gmail.com>
2023-11-18 16:01:36 +0800

    喵喵喵

--- src/main.ts
+++ src/main.ts
@@ -25,16 +25,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* html */ `
         </div>
       </div>
     </div>
-    <div class="disclaimer">
-      <p>免责声明：</p>
-      <p>
-        本网站是一个纯粹的技术示例，旨在展示和分享我们的技术能力。网站的设计和内容受到《原神》的启发，并尽可能地复制了《原神》的登录界面。我们对此表示敬意，并强调这个项目不是官方的《原神》产品，也没有与《原神》或其母公司miHoYo有任何关联。
-      </p>
-      <p>
-        我们没有，也无意从这个项目中获得任何经济利益。这个网站的所有内容仅供学习和研究目的，以便让更多的人了解和熟悉webgl开发技术。
-      </p>
-      <p>如果miHoYo或任何有关方面认为这个项目侵犯了他们的权益，请联系我们，我们会立即采取行动。</p>
-    </div>
+
   </div>
 </div>
 <div class="enter fixed z-4 top-0 left-0 w-full h-full transition-opacity duration-500 select-none hidden">
@@ -58,7 +49,7 @@ const app = document.querySelector("#app")! as HTMLElement;
 
 adaptMobileDOM(app);
 window.addEventListener("resize", () => {
-  adaptMobileDOM(app);
+    adaptMobileDOM(app);
 });
 
 new Experience("#sketch");
